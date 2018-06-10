baf5b0bde568dea3c294b1ddbae17e76734f2eb4
Bast Krombholz <bast1@outlook.com>
2018-06-10 14:42:05 +0200

    Klasse im Grund Beginn mit Auswahl

--- Lagerverwaltung/Lagerverwaltung/Program.cs
+++ Lagerverwaltung/Lagerverwaltung/Program.cs
@@ -6,10 +6,65 @@ using System.Threading.Tasks;
 
 namespace Lagerverwaltung
 {
+    public class Artikel
+    {
+        public int nummer { get; private set; }
+        private double menge;
+        public string bezeichnung { get; private set; }
+        private int platz;
+
+        public double Menge
+        {
+            get
+            {
+                return menge;
+            }
+            private set
+            {
+                menge = value;
+            }
+        }
+
+        public Artikel(int _nummer, double _menge, string Bezeichnung, int _platz = 0)
+        {
+            menge = _menge;
+            bezeichnung = Bezeichnung;
+            nummer = _nummer;
+            platz = _platz;
+        }
+
+        public void mengeAendern(double mengeNeu)
+        {
+                menge += mengeNeu;
+        }
+
+        public int getPlatz()
+        {
+            return platz;
+        }
+
+        public void platzAendern()
+        {
+            if (platz > 4)
+            {
+                platz = 0;
+            }
+            else
+            {
+                platz += 1;
+            }
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Lagerverwaltung 0.0001 Alpha");
+            Artikel Apfel = new Artikel(001, 2, "Apfel");
+            int Auswahl = Convert.ToInt32(Console.ReadLine());
+            
         }
     }
 }
